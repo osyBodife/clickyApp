2a469195aca6d5796260ef7b2d78c6fca35f2c40
Osy odife <osyodife@gmail.com>
2019-10-12 20:42:45 -0400

    upload changes to file and folders

--- click-app/src/App.js
+++ click-app/src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import friends from "./friends.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    TopScore: 0,
+    Score: 0,
+    Message: "",
+    CardVal: 1
+  };
+
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
+
+  handleArrayShuffle = () => {    
+    const newfriends = this.state.friends.sort(() => Math.random() - 0.5);
+    this.setState({ newfriends });
+  };
+
+  handleClicky = () => {
+    if (this.state.CardValue === 1) {
+      this.setState({
+        TopScore: this.state.TopScore + 1,
+        Score: this.state.Score + 1,
+        CardValue: (this.state.CardValue -1)
+      
+      });
+    } else {
+      this.setState({
+        Score: (this.state.Score - this.state.Score),
+        CardValue: (this.state.CardValue + 1)
+      });
+    }
+    this.handleArrayShuffle();
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Header>
+          <ul>
+            <li>Clicky Game</li>
+
+            <li>Click on image to begin</li>
+            <li>
+              Score:{this.state.Score} | Top Score :{this.state.TopScore}
+            </li>
+          </ul>
+        </Header>
+        <button onClick={this.handleClicky} >
+        {this.state.friends.map(friend => (                   
+         
+          <FriendCard
+           
+            id={friend.id}
+            key={friend.id}  
+            image={friend.image}
+            
+          />
+        ))}
+        </button>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
